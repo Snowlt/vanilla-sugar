87d2065e141a08cbd85f3081a042dacecbc37105
Snowlt <98334270+Snowlt@users.noreply.github.com>
2024-05-04 16:32:23 +0800

    补充IniReaderWriter入口类中的Javadoc说明

--- vanilla-sugar-ini/src/main/java/xyz/udw/sugar/ini/IniReaderWriter.java
+++ vanilla-sugar-ini/src/main/java/xyz/udw/sugar/ini/IniReaderWriter.java
@@ -20,10 +20,25 @@ public class IniReaderWriter {
             .setAddSpaceAroundEqualizer(true).setAddSpaceBeforeComment(true);
     private static final IniDeserializer FILE_READER = new IniDeserializer();
 
+    /**
+     * 从文件读取解析并返回为 INI 对象。
+     *
+     * @param path 文件路径
+     * @return 读取出的 INI
+     * @throws ReadWriteException 当IO异常时抛出
+     */
     public static Ini loadFromFile(String path) {
         return loadFromFile(path, StandardCharsets.UTF_8);
     }
 
+    /**
+     * 从文件读取解析并返回为 INI 对象。
+     *
+     * @param path 文件路径
+     * @param charset 文件的字符集
+     * @return 读取出的 INI
+     * @throws ReadWriteException 当IO异常时抛出
+     */
     public static Ini loadFromFile(String path, Charset charset) {
         try (FileInputStream stream = new FileInputStream(path)){
             return FILE_READER.read(stream, charset);
@@ -32,10 +47,25 @@ public class IniReaderWriter {
         }
     }
 
+    /**
+     * 将 INI 的内容保存到文件中。
+     *
+     * @param ini  要保存的 INI 对象
+     * @param path 文件路径
+     * @throws ReadWriteException 当IO异常时抛出
+     */
     public static void saveToFile(Ini ini, String path) {
         saveToFile(ini,path,StandardCharsets.UTF_8);
     }
 
+    /**
+     * 将 INI 的内容保存到文件中。
+     *
+     * @param ini     要保存的 INI 对象
+     * @param path 文件路径
+     * @param charset 文件的字符集
+     * @throws ReadWriteException 当IO异常时抛出
+     */
     public static void saveToFile(Ini ini, String path, Charset charset) {
         try (FileOutputStream stream = new FileOutputStream(path)) {
             FILE_WRITER.write(ini, stream, charset);
@@ -44,10 +74,25 @@ public class IniReaderWriter {
         }
     }
 
+    /**
+     * 将 INI 的内容保存到文件中。
+     *
+     * @param ini  要保存的 INI 对象
+     * @param path 文件路径
+     * @throws ReadWriteException 当IO异常时抛出
+     */
     public static void saveToPrettyFile(Ini ini, String path) {
         saveToPrettyFile(ini,path,StandardCharsets.UTF_8);
     }
 
+    /**
+     * 将 INI 的内容保存到文件中。
+     *
+     * @param ini     要保存的 INI 对象
+     * @param path 文件路径
+     * @param charset 文件的字符集
+     * @throws ReadWriteException 当IO异常时抛出
+     */
     public static void saveToPrettyFile(Ini ini, String path, Charset charset) {
         try (FileOutputStream stream = new FileOutputStream(path)) {
             PRETTY_FILE_WRITER.write(ini, stream, charset);
